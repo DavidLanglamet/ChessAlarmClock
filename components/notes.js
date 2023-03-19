f580fac835603473f5240474cf84d216f87ad924
David Langlamet <david.langlamet@gmail.com>
2023-03-19 13:58:14 +0100

    added notes file

---
+++ components/notes.js
@@ -0,0 +1,28 @@
+// TODO:
+
+// Check if pressing + twice quickly causes problems
+// add a little v at the top of the modal to make it clear that you can swipe down and discard changes.
+
+
+
+
+
+
+
+
+
+// AlarmSettings links:
+
+// next link is for dropdown picker issues
+// https://stackoverflow.com/questions/67573201/react-native-dropdown-picker-how-to-fix-the-dropdown-picker-overlay-on-other-co
+// https://blog.logrocket.com/react-native-gesture-handler-swipe-long-press-and-more/
+// https://stackoverflow.com/questions/4147046/is-it-possible-to-remove-am-pm-button-from-timepicker
+// https://github.com/hoaphantn7604/react-native-element-dropdown
+// timePicker for android:
+// https://stackoverflow.com/questions/58925515/using-react-native-community-datetimepicker-how-can-i-display-a-datetime-picker
+
+
+
+
+
+// switch colors  8ada6f     3e9950    47FF2E
